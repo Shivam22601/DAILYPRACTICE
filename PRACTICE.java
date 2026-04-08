f2a06e1ee453eac9b181b97ac8f08925dd518d20
Shivam <mishrashivam20197@gmail.com>
2026-04-08 20:39:49 +0530

    search insert

--- PRACTICE.java
+++ PRACTICE.java
@@ -812,13 +812,42 @@
 // }
 
 // binary search
+// public class PRACTICE{
+//     public int binarySearch(int[] nums, int target){
+//         int low = 0;
+//         int high = nums.length - 1;
+//         while(low <= high){
+//             int mid = low + (high - low) / 2;
+//             if(nums[mid] == target){
+//                 return mid;
+//             }
+//             else if(nums[mid] < target){
+//                 low = mid + 1;
+//             }
+//             else{
+//                 high = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+//     public static void main(String[] args) {
+//         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//         int target = 5;
+//         PRACTICE practice = new PRACTICE();
+//         int result = practice.binarySearch(nums, target);
+//         System.out.println("Target index: " + result);
+//     }
+// }
+
+//search insert
 public class PRACTICE{
-    public int binarySearch(int[] nums, int target){
+   public int searchInsert(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
+
         while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target){
+            int mid = low + (high - low)/2;
+            if(nums[mid] ==  target){
                 return mid;
             }
             else if(nums[mid] < target){
@@ -828,13 +857,14 @@ public class PRACTICE{
                 high = mid - 1;
             }
         }
-        return -1;
+        return low;
     }
-    public static void main(String[] args) {
+    
+    public static void main(String[] args){
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int target = 5;
         PRACTICE practice = new PRACTICE();
-        int result = practice.binarySearch(nums, target);
+        int result = practice.searchInsert(nums, target);
         System.out.println("Target index: " + result);
     }
 }
