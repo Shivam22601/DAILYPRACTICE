66085ad1349be326e0de8bd46e2b925b8dcb7a26
Shivam <mishrashivam20197@gmail.com>
2026-04-13 21:18:59 +0530

    intersection of two arrays

--- PRACTICE.java
+++ PRACTICE.java
@@ -1,19 +1,52 @@
 //Contains Duplicate
+// import java.util.HashSet;
+// public class PRACTICE{
+//     public static boolean containsDuplicate(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for (int num : nums) {
+//             if (set.contains(num)) {
+//                 return true; // Duplicate found
+//             }
+//             set.add(num);
+//         }
+//         return false; // No duplicates found
+//     }
+
+//     public static void main(String[] args) {
+//         int[] nums = {1, 2, 3, 4, 5, 2};
+//         System.out.println(containsDuplicate(nums));
+//     }
+// }
+
+//intersection of two arrays
 import java.util.HashSet;
 public class PRACTICE{
-    public static boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (set.contains(num)) {
-                return true; // Duplicate found
-            }
-            set.add(num);
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        for (int num : nums1){
+            set1.add(num);
         }
-        return false; // No duplicates found
+        HashSet<Integer> result = new HashSet<>();
+        for(int num :nums2){
+            if(set1.contains(num)){
+                result.add(num);
+            }
+        }
+        int[] ans = new int[result.size()];
+        int index = 0;
+        for(int val : result){
+            ans[index++] = val;
+        }
+        return ans;
     }
 
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 2};
-        System.out.println(containsDuplicate(nums));
+    public static void main(String[] args){
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        PRACTICE p = new PRACTICE();
+        int[] result = p.intersection(nums1, nums2);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
     }
 }
