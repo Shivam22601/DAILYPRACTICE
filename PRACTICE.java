94c37bc7c2e006743c3ef252e1ab847ac540565b
Shivam <mishrashivam20197@gmail.com>
2026-04-27 12:19:10 +0530

    contains duplicate

--- PRACTICE.java
+++ PRACTICE.java
@@ -31,27 +31,54 @@
 
 
 //maximum sum subarray
+// public class PRACTICE{
+//     public static int maxSumSubarray(int[] arr, int k){
+//         int maxSum = 0;
+//         int windowSum = 0;
+//         for(int i = 0 ; i<k ; i++){
+//             windowSum += arr[i];
+//         }
+//         maxSum = windowSum;
+//         for(int i = k; i<arr.length ; i++){
+//             windowSum += arr[i];
+//             windowSum -= arr[i - k];
+//             maxSum = Math.max(maxSum , windowSum);
+//         }
+//         return maxSum;
+//     }
+
+
+//     public static void main(String[] args){
+//          int[] arr = {2, 1, 5, 1, 3, 2};
+//         int k = 3;
+
+//         System.out.println(maxSumSubarray(arr, k));
+//     }
+// }
+
+//contains duplicate II
+import java.util.HashSet;
 public class PRACTICE{
-    public static int maxSumSubarray(int[] arr, int k){
-        int maxSum = 0;
-        int windowSum = 0;
-        for(int i = 0 ; i<k ; i++){
-            windowSum += arr[i];
+    public boolean containsDuplicate(int[] nums, int k){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i <nums.length ; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+
+            set.add(nums[i]);
+
+            if(set.size() > k){
+                set.remove(nums[i-k]);
+            }
         }
-        maxSum = windowSum;
-        for(int i = k; i<arr.length ; i++){
-            windowSum += arr[i];
-            windowSum -= arr[i - k];
-            maxSum = Math.max(maxSum , windowSum);
-        }
-        return maxSum;
+        return false;
     }
 
-
-    public static void main(String[] args){
-         int[] arr = {2, 1, 5, 1, 3, 2};
+     public static void main(String[] args){
+         int[] nums = {2, 1, 5, 3, 2};
         int k = 3;
-
-        System.out.println(maxSumSubarray(arr, k));
+        PRACTICE obj = new PRACTICE();
+        System.out.println(obj.containsDuplicate(nums, k));
     }
 }
