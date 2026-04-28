fa91a4f2d41ac8bbb02a9cc936bf2741e308e640
Shivam <mishrashivam20197@gmail.com>
2026-04-28 21:10:25 +0530

    two sum by hashmap

--- PRACTICE.java
+++ PRACTICE.java
@@ -28,15 +28,58 @@
 //  }
 
 
+// public class PRACTICE{
+//     public int[] twosum(int[] nums, int target) {
+//       for(int i =0; i< nums.length; i++){
+//         for(int j = i+1; j< nums.length; j++){
+//             if(nums[i] + nums[j] == target){
+//                 return new int[]{i, j};
+//             }
+//         }
+//       }
+//       return new int[]{-1, -1};
+//     }
+
+//     public static void main(String[] args) {
+//         PRACTICE practice = new PRACTICE();
+//         int[] nums = {2, 7, 11, 15};
+//         int target = 9;
+//         int[] result = practice.twosum(nums, target);
+//         System.out.println("Indices: " + result[0] + ", " + result[1]);
+//     }
+//  }
+
+// public class PRACTICE{
+//     public int[] twosum(int[] nums, int target) {
+//       for(int i =0; i< nums.length; i++){
+//         for(int j = i+1; j< nums.length; j++){
+//             if(nums[i] + nums[j] == target){
+//                 return new int[]{i, j};
+//             }
+//         }
+//       }
+//       return new int[]{-1, -1};
+//     }
+
+//     public static void main(String[] args) {
+//         PRACTICE practice = new PRACTICE();
+//         int[] nums = {2, 7, 11, 15};
+//         int target = 9;
+//         int[] result = practice.twosum(nums, target);
+//         System.out.println("Indices: " + result[0] + ", " + result[1]);
+//     }
+//  }
+import java.util.HashMap;
 public class PRACTICE{
     public int[] twosum(int[] nums, int target) {
-      for(int i =0; i< nums.length; i++){
-        for(int j = i+1; j< nums.length; j++){
-            if(nums[i] + nums[j] == target){
-                return new int[]{i, j};
-            }
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for(int i = 0; i<nums.length ; i++){
+        int remaining = target - nums[i];
+        if(map.containsKey(remaining)){
+            return new int[]{map.get(remaining),i};
         }
-      }
+         map.put(nums[i], i);
+    }
       return new int[]{-1, -1};
     }
 
