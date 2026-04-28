7dfb6c6bcd7624295f44103a86a483ff86fba079
Shivam <mishrashivam20197@gmail.com>
2026-04-28 21:05:41 +0530

    two sum by using brute force

--- PRACTICE.java
+++ PRACTICE.java
@@ -1,21 +1,43 @@
 // two pointers
 
 //two sum
+// public class PRACTICE{
+//     public int[] twosum(int[] nums, int target) {
+//         int left = 0;
+//         int right = nums.length - 1;
+//         while (left < right) {
+//             int sum = nums[left] + nums[right];
+//             if (sum == target) {
+//                 return new int[]{left, right};
+//             } else if (sum < target) {
+//                 left++;
+//             } else {
+//                 right--;
+//             }
+//         }
+//         return new int[]{-1, -1}; // not found
+//     }
+
+//     public static void main(String[] args) {
+//         PRACTICE practice = new PRACTICE();
+//         int[] nums = {2, 7, 11, 15};
+//         int target = 9;
+//         int[] result = practice.twosum(nums, target);
+//         System.out.println("Indices: " + result[0] + ", " + result[1]);
+//     }
+//  }
+
+
 public class PRACTICE{
     public int[] twosum(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            int sum = nums[left] + nums[right];
-            if (sum == target) {
-                return new int[]{left, right};
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
+      for(int i =0; i< nums.length; i++){
+        for(int j = i+1; j< nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return new int[]{i, j};
             }
         }
-        return new int[]{-1, -1}; // not found
+      }
+      return new int[]{-1, -1};
     }
 
     public static void main(String[] args) {
