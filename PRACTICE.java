0e40308d835bbf11eff70d651df5b3cbfd1cfd5a
Shivam <mishrashivam20197@gmail.com>
2026-04-28 21:03:51 +0530

    two sum ny two pointers for sorted array

--- PRACTICE.java
+++ PRACTICE.java
@@ -1,84 +1,28 @@
-// sliding window
-//longest substring without repeating charcter
+// two pointers
 
-// import java.util.HashSet;
-// import java.util.*;
-// public class PRACTICE{
-//     public int strng(String s){
-//         int left = 0;
-//         int maxlen = 0;
-//         Set <Character> charset = new HashSet<>();
-//         for(int right = 0 ; right <s.length() ; right++){
-//             char current = s.charAt(right);
-
-//             while(charset.contains(current)){
-//                 charset.remove(s.charAt(left));
-//                 left++;
-//             }
-//             charset.add(current);
-//             maxlen = Math.max(maxlen, right - left + 1);
-//         }
-//         return maxlen;
-//     }
-
-//     public static void main(String[] args) {
-//         String s = "abcabcbb";
-//         PRACTICE obj = new PRACTICE();
-//         int k = obj.strng(s);
-//         System.out.print("length of substring is : " + k);
-//     }
-// }
-
-
-//maximum sum subarray
-// public class PRACTICE{
-//     public static int maxSumSubarray(int[] arr, int k){
-//         int maxSum = 0;
-//         int windowSum = 0;
-//         for(int i = 0 ; i<k ; i++){
-//             windowSum += arr[i];
-//         }
-//         maxSum = windowSum;
-//         for(int i = k; i<arr.length ; i++){
-//             windowSum += arr[i];
-//             windowSum -= arr[i - k];
-//             maxSum = Math.max(maxSum , windowSum);
-//         }
-//         return maxSum;
-//     }
-
-
-//     public static void main(String[] args){
-//          int[] arr = {2, 1, 5, 1, 3, 2};
-//         int k = 3;
-
-//         System.out.println(maxSumSubarray(arr, k));
-//     }
-// }
-
-//contains duplicate II
-import java.util.HashSet;
+//two sum
 public class PRACTICE{
-    public boolean containsDuplicate(int[] nums, int k){
-        HashSet<Integer> set = new HashSet<>();
-        for(int i = 0; i <nums.length ; i++){
-            if(set.contains(nums[i])){
-                return true;
-            }
-
-            set.add(nums[i]);
-
-            if(set.size() > k){
-                set.remove(nums[i-k]);
+    public int[] twosum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{left, right};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return false;
+        return new int[]{-1, -1}; // not found
     }
 
-     public static void main(String[] args){
-         int[] nums = {2, 1, 5, 3, 2};
-        int k = 3;
-        PRACTICE obj = new PRACTICE();
-        System.out.println(obj.containsDuplicate(nums, k));
+    public static void main(String[] args) {
+        PRACTICE practice = new PRACTICE();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = practice.twosum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
     }
-}
+ }
