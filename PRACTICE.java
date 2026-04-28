a13bd1f1bfc6e67c3d87e0688724df9297a95ce6
Shivam <mishrashivam20197@gmail.com>
2026-04-28 21:29:53 +0530

    first unique character in a string

--- PRACTICE.java
+++ PRACTICE.java
@@ -69,25 +69,52 @@
 //         System.out.println("Indices: " + result[0] + ", " + result[1]);
 //     }
 //  }
-import java.util.HashMap;
+// import java.util.HashMap;
+// public class PRACTICE{
+//     public int[] twosum(int[] nums, int target) {
+//     HashMap<Integer, Integer> map = new HashMap<>();
+//     for(int i = 0; i<nums.length ; i++){
+//         int remaining = target - nums[i];
+//         if(map.containsKey(remaining)){
+//             return new int[]{map.get(remaining),i};
+//         }
+//          map.put(nums[i], i);
+//     }
+//       return new int[]{-1, -1};
+//     }
+
+//     public static void main(String[] args) {
+//         PRACTICE practice = new PRACTICE();
+//         int[] nums = {2, 7, 11, 15};
+//         int target = 9;
+//         int[] result = practice.twosum(nums, target);
+//         System.out.println("Indices: " + result[0] + ", " + result[1]);
+//     }
+//  }
+
+
+// first unique char in a string
+
 public class PRACTICE{
-    public int[] twosum(int[] nums, int target) {
-    HashMap<Integer, Integer> map = new HashMap<>();
-    for(int i = 0; i<nums.length ; i++){
-        int remaining = target - nums[i];
-        if(map.containsKey(remaining)){
-            return new int[]{map.get(remaining),i};
+    public int firstunique(String s){
+        int[] freq = new int[26];
+
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
         }
-         map.put(nums[i], i);
-    }
-      return new int[]{-1, -1};
+
+        for(int i= 0; i <s.length() ; i++){
+            if(freq[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
         PRACTICE practice = new PRACTICE();
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        int[] result = practice.twosum(nums, target);
-        System.out.println("Indices: " + result[0] + ", " + result[1]);
+        String s = "leetcode";
+        int result = practice.firstunique(s);
+        System.out.println("First unique character index: " + result);
     }
- }
+}
