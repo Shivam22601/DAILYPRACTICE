a81a988aa38a5c9311d2c18e4fbb55d6c831c889
Shivam <mishrashivam20197@gmail.com>
2026-04-30 01:08:26 +0530

    reverse linked list

--- PRACTICE.java
+++ PRACTICE.java
@@ -37,16 +37,46 @@ class ListNode{
 
    
 // }
-public class PRACTICE {
+// public class PRACTICE {
 
-    public static ListNode middle(ListNode head){
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+//     public static ListNode middle(ListNode head){
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while(fast != null && fast.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         return slow;
+//     }
+//     public static void main(String[] args) {
+//         ListNode head = new ListNode(1);
+//         head.next = new ListNode(2);
+//         head.next.next = new ListNode(3);
+//         head.next.next.next = new ListNode(4);
+//         head.next.next.next.next = new ListNode(5);
+
+        
+
+//         System.out.print(middle(head).val);
+        
+//     }
+
+   
+// }
+
+
+// reverse linked list
+public class PRACTICE{
+    public static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode nexttemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nexttemp;
         }
-        return slow;
+        return prev;
     }
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
@@ -57,9 +87,7 @@ public class PRACTICE {
 
         
 
-        System.out.print(middle(head).val);
+        System.out.print(reverse(head).val);
         
     }
-
-   
 }
