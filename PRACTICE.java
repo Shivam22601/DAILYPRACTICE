2ea6511cefeda2d5edfdee30d1ea1f1dd801dc28
Shivam <mishrashivam20197@gmail.com>
2026-04-30 23:19:50 +0530

    maximum product subarray

--- PRACTICE.java
+++ PRACTICE.java
@@ -24,29 +24,60 @@
 
 // subarray sum equals k
 
+// public class PRACTICE{
+//     public int subarraySum(int[] nums, int k) {
+//         int count = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             int currentSum = 0;
+
+//             for (int j = i; j < nums.length; j++) {
+//                 currentSum += nums[j];
+
+//                 if (currentSum == k) {
+//                     count++;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] nums = {1, 1, 1};
+//         int k = 2;
+//         PRACTICE p = new PRACTICE();
+//         System.out.println(p.subarraySum(nums, k));
+//     }
+// }
+
+//maximum product subarray
 public class PRACTICE{
-    public int subarraySum(int[] nums, int k) {
-        int count = 0;
+    public int maxProduct(int[] nums) {
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int ans = nums[0];
 
-        for (int i = 0; i < nums.length; i++) {
-            int currentSum = 0;
-
-            for (int j = i; j < nums.length; j++) {
-                currentSum += nums[j];
-
-                if (currentSum == k) {
-                    count++;
-                }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                // Swap maxProduct and minProduct
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
             }
+
+            maxProduct = Math.max(nums[i], maxProduct * nums[i]);
+            minProduct = Math.min(nums[i], minProduct * nums[i]);
+
+            ans = Math.max(ans, maxProduct);
         }
 
-        return count;
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 1, 1};
-        int k = 2;
+        int[] nums = {2, 3, -2, 4};
         PRACTICE p = new PRACTICE();
-        System.out.println(p.subarraySum(nums, k));
+        System.out.println(p.maxProduct(nums));
     }
 }
