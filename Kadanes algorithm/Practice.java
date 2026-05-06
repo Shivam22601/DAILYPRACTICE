e2e3e2902a78eebc9d7956ac89b943f449b3df11
Shivam <mishrashivam20197@gmail.com>
2026-05-06 21:46:18 +0530

    max Sub array

---
+++ Kadanes algorithm/Practice.java
@@ -0,0 +1,21 @@
+public class Practice{
+     public int maxSubArray(int[] nums) {
+        int bestending = nums[0];
+        int ans = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            int v1 = bestending + nums[i];
+            int v2 = nums[i];
+            bestending = Math.max(v1, v2);
+            ans = Math.max(ans, bestending);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Practice p = new Practice();
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(p.maxSubArray(nums));
+    }
+
+}
