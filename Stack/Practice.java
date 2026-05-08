59fd2950572f24c17c2f1f5ebefc952daaee31d3
Shivam <mishrashivam20197@gmail.com>
2026-05-08 21:04:29 +0530

    reverse string

---
+++ Stack/Practice.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+// import java.util.String;
+public class Practice{
+    public static String reverse(String str){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < str.length() ;i++){
+            stack.push(str.charAt(i));
+        }
+        String reversed = "";
+        while(!stack.isEmpty()){
+            reversed += stack.pop();
+        }
+        return reversed;
+    }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        String str = "Hello";
+        String result = reverse(str);
+
+        System.out.println("Reversed string: " + result);
+
+        
+    }
+}
---
+++ Stack/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+ // System.out.println("Top element: " + stack.peek());
+        // System.out.println("Popped element: " + stack.pop());
+        // System.out.println("Stack after pop: " + stack);
