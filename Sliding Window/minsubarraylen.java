3681b094c8bcd3251425513cd71366e2f6dcc9ad
Shivam <mishrashivam20197@gmail.com>
2026-05-18 20:55:57 +0530

    min size subarray sum equals k

---
+++ Sliding Window/minsubarraylen.java
@@ -0,0 +1,27 @@
+public class minsubarraylen{
+    public int minsubarraylen(int target, int[] nums){
+        int n = nums.length;
+        int low = 0;
+        int min_len = Integer.MAX_VALUE;
+        int sum = 0;
+        for(int high = 0; high < n; high++){
+            sum+=nums[high];
+            while(sum>=target){
+                min_len = Math.min(min_len, high-low+1);
+                sum-=nums[low];
+                low++;
+            }
+        } 
+        if(min_len == Integer.MAX_VALUE){
+            return 0;
+        }else{
+            return min_len;
+        }
+    }
+    public static void main(String[] args) {
+        minsubarraylen obj = new minsubarraylen();
+        int target = 7;
+        int[] nums = {2,3,1,2,4,3};
+        System.out.println(obj.minsubarraylen(target, nums));        
+    }
+}
