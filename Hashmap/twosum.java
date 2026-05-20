e70041b81e915acd1882775f691350b747f1e3fa
Shivam <mishrashivam20197@gmail.com>
2026-05-20 21:32:59 +0530

    two sum by using hashmap for sorted array

---
+++ Hashmap/twosum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+public class twosum{
+    public static int[] twosum(int[] nums, int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length ; i++){
+            int remaining = target - nums[i];
+            if(map.containsKey(remaining)){
+                return new int[]{remaining, nums[i]};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args){
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twosum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
