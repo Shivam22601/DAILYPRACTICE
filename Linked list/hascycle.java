4090a77a13d535aa14f2de09ea6e0c151cc4c861
Shivam <mishrashivam20197@gmail.com>
2026-05-21 20:50:28 +0530

    hascycle

---
+++ Linked list/hascycle.java
@@ -0,0 +1,40 @@
+class ListNode{
+     int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+public class hascycle{
+    public boolean hascycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(7);
+        head.next.next = new ListNode(5);
+        head.next.next.next = new ListNode(2);
+        // head.next.next.next.next = new ListNode(1);
+
+        // Creating cycle
+        head.next.next.next.next = head; // Creates a cycle
+
+        hascycle obj = new hascycle();
+        boolean result = obj.hasCycle(head);
+        System.out.println("Does the linked list have a cycle? " + result);
+    }
+}
