3f71116d6ddb04e180945077810d3927b30abf33
Shivam <mishrashivam20197@gmail.com>
2026-05-21 20:55:04 +0530

    updat

--- Linked list/Practice.java
+++ Linked list/Practice.java
@@ -10,18 +10,7 @@ class ListNode {
 
 public class Practice {
 
-    public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) return false;
-
-        ListNode slow = head, fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if (slow == fast) return true;
-        }
-        return false;
+   
     }
 
     public ListNode middleNode(ListNode head){
