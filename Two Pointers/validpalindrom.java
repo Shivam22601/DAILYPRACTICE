862abc4b5b3f369ff6b4d536b0be56f6c4f8c368
Shivam <mishrashivam20197@gmail.com>
2026-05-24 18:58:13 +0530

    valid palindrome

--- Two Pointers/validpalindrom.java
+++ Two Pointers/validpalindrom.java
@@ -1,6 +1,15 @@
 public class validpalindrom{
     public boolean ispalisndrome(String s){
-        
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
     public static void main(String[] args){
         String s = "abccba";
