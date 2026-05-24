3e435509a953125945bdc13538242a3526608b60
Shivam <mishrashivam20197@gmail.com>
2026-05-24 18:42:44 +0530

    update

--- Two Pointers/twosum.java
+++ Two Pointers/twosum.java
@@ -1,23 +1,9 @@
 import java.util.Arrays;
 public class twosum{
     public int[] twosum(int[] nums, int target){
-        int left = 0;
-        int right = nums.length - 1;
-
-        while(left <= right){
-            int sum = nums[left] + nums[right];
-            if(sum == target){
-                return new int[]{left, right};
-            }
-            else if(sum < target){  
-                left++;
-            }else{
-                right--;
-            }
-        }
-        return new int[]{-1,-1};
 
     }
+    
     public static void main(String[] args){
         int[] nums = new int[]{2,3,7,11};
         int target = 9;
