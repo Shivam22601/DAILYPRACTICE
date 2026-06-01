f8ef86e9f4ed4ec3671f9339ebcfd23dcf77ed43
Shivam <mishrashivam20197@gmail.com>
2026-06-01 20:49:17 +0530

    update

--- Two Pointers/removeduplicates.java
+++ Two Pointers/removeduplicates.java
@@ -1,37 +1,37 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 
-public class removeduplicates {
+// public class removeduplicates {
 
-    public int[] removeduplicate(int[] nums) {
+//     public int[] removeduplicate(int[] nums) {
 
-        int i = 0;
+//         int i = 0;
 
-        for (int j = 0; j < nums.length; j++) {
-            int k;
-            for (k = 0; k < i; k++) {
-                if (nums[j] == nums[k]) {
-                    break;
-                }
-            }
+//         for (int j = 0; j < nums.length; j++) {
+//             int k;
+//             for (k = 0; k < i; k++) {
+//                 if (nums[j] == nums[k]) {
+//                     break;
+//                 }
+//             }
 
-            // if not found, add it
-            if (k == i) {
-                nums[i] = nums[j];
-                i++;
-            }
-        }
+//             // if not found, add it
+//             if (k == i) {
+//                 nums[i] = nums[j];
+//                 i++;
+//             }
+//         }
 
-        return Arrays.copyOf(nums, i);
-    }
+//         return Arrays.copyOf(nums, i);
+//     }
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        removeduplicates solution = new removeduplicates();
+//         removeduplicates solution = new removeduplicates();
 
-        int[] nums = {2, 8, 3, 7, 6, 7, 11, 15};
+//         int[] nums = {2, 8, 3, 7, 6, 7, 11, 15};
 
-        int[] result = solution.removeduplicate(nums);
+//         int[] result = solution.removeduplicate(nums);
 
-        System.out.println("Removed Duplicates: " + Arrays.toString(result));
-    }
-}
+//         System.out.println("Removed Duplicates: " + Arrays.toString(result));
+//     }
+// }
