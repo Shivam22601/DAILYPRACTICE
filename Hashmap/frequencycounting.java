af540e4628e79abad0e2a5cf50fff196cc14c6c8
Shivam <mishrashivam20197@gmail.com>
2026-06-05 21:59:34 +0530

    frequency counting

--- Hashmap/frequencycounting.java
+++ Hashmap/frequencycounting.java
@@ -1,19 +1,18 @@
-// import java.util.HashMap;
-// public class frequencycounting{
-//     public static HashMap<Integer, Integer> frequencycounting(int arr[]){
-//         HashMap<Integer, Integer> map = new HashMap<>();
-//         for(int i = 0; i < arr.length; i++){
-//             if(map.containsKey(arr[i])){
-//                 map.put(arr[i], map.get(arr[i]) + 1);
-//             } else {
-//                 map.put(arr[i], 1);
-//             }
-//         }
-//         return map;
-//     }
-//     public static void main(String[] args) {
-//         int arr[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
-//         HashMap<Integer, Integer> freq = frequencycounting(arr);
-//         System.out.println(freq);
-//     }
-// }
+import java.util.HashMap;
+public class frequencycounting{
+    public static HashMap<Integer, Integer> frequencycounting(int arr[]){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        return map;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+        HashMap<Integer, Integer> freq = frequencycounting(arr);
+        System.out.println(freq);
+    }
