97fe37eb3db5d4a1869eaf27e209d4e0b026a3a7
Shivam <mishrashivam20197@gmail.com>
2026-06-10 20:25:34 +0530

    update

--- Sliding Window/maxsum.java
+++ Sliding Window/maxsum.java
@@ -1,25 +1,25 @@
-public class maxsum{
-    public static int maxsumsubarray(int k, int[] nums){
-        int n = nums.length;
-        if(k>n){
-            return -1;
-        }
-        int sum = 0;
-        for(int i = 0; i<k; i++){
-            sum += nums[i];
-        }
+// public class maxsum{
+//     public static int maxsumsubarray(int k, int[] nums){
+//         int n = nums.length;
+//         if(k>n){
+//             return -1;
+//         }
+//         int sum = 0;
+//         for(int i = 0; i<k; i++){
+//             sum += nums[i];
+//         }
 
-        int maxsum = sum;
+//         int maxsum = sum;
 
-        for(int i =k ; i<n ; i++){
-            sum = sum - nums[i-k] + nums[i];
-            maxsum = Math.max(maxsum, sum);
-        }
-        return maxsum;
-    }
-    public static void main(String[] args) {
-        int k = 3;
-        int[] nums = {2,1,5,1,3,2};
-        System.out.println(maxsumsubarray(k, nums));
-    }
-}
+//         for(int i =k ; i<n ; i++){
+//             sum = sum - nums[i-k] + nums[i];
+//             maxsum = Math.max(maxsum, sum);
+//         }
+//         return maxsum;
+//     }
+//     public static void main(String[] args) {
+//         int k = 3;
+//         int[] nums = {2,1,5,1,3,2};
+//         System.out.println(maxsumsubarray(k, nums));
+//     }
+// }
