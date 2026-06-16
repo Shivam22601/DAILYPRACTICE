b9ce6a47b967751bfa0dd88d5b8941afff1ceacb
Shivam <mishrashivam20197@gmail.com>
2026-06-16 21:29:58 +0530

    update

--- Sliding Window/longestsubstring.java
+++ Sliding Window/longestsubstring.java
@@ -2,22 +2,22 @@ import java.util.HashSet;
 import java.util.Set;
 public class longestsubstring{
      public int lengthOfLongestSubstring(String s) {
-        int maxLen = 0;
-        int left = 0;
+        // int maxLen = 0;
+        // int left = 0;
         
-        Set<Character> charSet = new HashSet<>();
+        // Set<Character> charSet = new HashSet<>();
         
-        for (int right = 0; right < s.length(); right++) {
-            char currentChar = s.charAt(right);
-            while (charSet.contains(currentChar)) {
-                charSet.remove(s.charAt(left));
-                left++;
-            }
-            charSet.add(currentChar);
-            maxLen = Math.max(maxLen, right - left + 1);
-        }
+        // for (int right = 0; right < s.length(); right++) {
+        //     char currentChar = s.charAt(right);
+        //     while (charSet.contains(currentChar)) {
+        //         charSet.remove(s.charAt(left));
+        //         left++;
+        //     }
+        //     charSet.add(currentChar);
+        //     maxLen = Math.max(maxLen, right - left + 1);
+        // }
         
-        return maxLen;
+        // return maxLen;
     }
     public static void main(String[] args) {
         longestsubstring solution = new longestsubstring();
