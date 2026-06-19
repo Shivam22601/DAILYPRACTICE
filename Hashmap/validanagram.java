981ec302a0a519c9fcd4c824b7018562572186db
Shivam <mishrashivam20197@gmail.com>
2026-06-19 22:00:25 +0530

    valid anagram

--- Hashmap/validanagram.java
+++ Hashmap/validanagram.java
@@ -1,28 +1,28 @@
-// import java.util.HashMap;
-// public class validanagram{
-//     public boolean isAnagram(String s, String t){
-//         if(s.length() != t.length()){
-//             return false;
-//         }
-//         HashMap<Character, Integer> map = new HashMap<>();
-//         for(char c : s.toCharArray()){
-//             map.put(c, map.getOrDefault(c, 0) + 1);
-//         }
-//         for(char c : t.toCharArray()){
-//             if(!map.containsKey(c) || map.get(c) == 0){
-//                 return false;
-//             }
-//             map.put(c, map.get(c) - 1);
-//         }
+import java.util.HashMap;
+public class validanagram{
+    public boolean isAnagram(String s, String t){
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for(char c : t.toCharArray()){
+            if(!map.containsKey(c) || map.get(c) == 0){
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
 
-//         return true;
-//     }
+        return true;
+    }
 
-//     public static void main(String[] args) {
-//         validanagram va = new validanagram();
-//         String s = "abcdef";
-//         String t = "listen";
-//         boolean result = va.isAnagram(s, t);
-//         System.out.println("Is anagram: " + result);
-//     }}
+    public static void main(String[] args) {
+        validanagram va = new validanagram();
+        String s = "abcdef";
+        String t = "listen";
+        boolean result = va.isAnagram(s, t);
+        System.out.println("Is anagram: " + result);
+    }}
 
