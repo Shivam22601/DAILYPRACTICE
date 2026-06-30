34d4a2654359a1af6cbff2e8b83447f0f6d105b6
Shivam <mishrashivam20197@gmail.com>
2026-06-30 22:07:31 +0530

    update

--- Two Pointers/movezeros.java
+++ Two Pointers/movezeros.java
@@ -1,24 +1,24 @@
-public class movezeros{
-    public void move(int[] nums){
-        int temp = 0;
-        for(int i = 0; i< nums.length; i++){
-            if(nums[i] != 0){
-                nums[temp] = nums[i];
-                temp++;
-            }
-        }
-        while(temp < nums.length){
-            nums[temp] = 0;
-            temp++;
-        }
-    }
-        public static void main(String[] args) {
-            movezeros m = new movezeros();
-            int[] nums = {0, 1, 0, 3, 12};
-            m.move(nums);
-            for(int i : nums){
-                System.out.print(i + " ");
-            }
-        }
-}
-}
+// public class movezeros{
+//     public void move(int[] nums){
+//         int temp = 0;
+//         for(int i = 0; i< nums.length; i++){
+//             if(nums[i] != 0){
+//                 nums[temp] = nums[i];
+//                 temp++;
+//             }
+//         }
+//         while(temp < nums.length){
+//             nums[temp] = 0;
+//             temp++;
+//         }
+//     }
+//         public static void main(String[] args) {
+//             movezeros m = new movezeros();
+//             int[] nums = {0, 1, 0, 3, 12};
+//             m.move(nums);
+//             for(int i : nums){
+//                 System.out.print(i + " ");
+//             }
+//         }
+// }
+// }
