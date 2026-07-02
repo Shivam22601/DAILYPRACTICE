f880043c6bbed90fac91159de406f9a1c3b50ea2
Shivam <mishrashivam20197@gmail.com>
2026-07-02 23:34:17 +0530

    update

--- Linked list/reverselist.java
+++ Linked list/reverselist.java
@@ -1,35 +1,35 @@
-class ListNode {
-    int val;
-    ListNode next;
+// class ListNode {
+//     int val;
+//     ListNode next;
 
-    ListNode(int val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-public class reverselist{
-    public ListNode reverselist(ListNode head){
-        ListNode prev = null;
-        ListNode curr = head;
+//     ListNode(int val) {
+//         this.val = val;
+//         this.next = null;
+//     }
+// }
+// public class reverselist{
+//     public ListNode reverselist(ListNode head){
+//         ListNode prev = null;
+//         ListNode curr = head;
 
-        while(curr != null){
-            ListNode nextTemp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextTemp;
-        }
-        return prev;
-    }
-    public static void main(String[] args){
-         ListNode head = new ListNode(3);
-        head.next = new ListNode(7);
-        head.next.next = new ListNode(5);
-        head.next.next.next = new ListNode(2);
-        reverselist obj = new reverselist();
-        ListNode reversedHead = obj.reverselist(head);
-        while (reversedHead != null) {
-            System.out.print(reversedHead.val + " ");
-            reversedHead = reversedHead.next;
-        }
-    }
-}
+//         while(curr != null){
+//             ListNode nextTemp = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = nextTemp;
+//         }
+//         return prev;
+//     }
+//     public static void main(String[] args){
+//          ListNode head = new ListNode(3);
+//         head.next = new ListNode(7);
+//         head.next.next = new ListNode(5);
+//         head.next.next.next = new ListNode(2);
+//         reverselist obj = new reverselist();
+//         ListNode reversedHead = obj.reverselist(head);
+//         while (reversedHead != null) {
+//             System.out.print(reversedHead.val + " ");
+//             reversedHead = reversedHead.next;
+//         }
+//     }
+// }
