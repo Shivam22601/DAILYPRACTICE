4d9f759bf19a00e1e5ac33c7b7007492a09080b0
Shivam <mishrashivam20197@gmail.com>
2026-07-06 20:44:46 +0530

    update

--- Two Pointers/validpalindrom.java
+++ Two Pointers/validpalindrom.java
@@ -1,19 +1,19 @@
-public class validpalindrom{
-    public boolean ispalisndrome(String s){
-        int left = 0;
-        int right = s.length() - 1;
-        while(left < right){
-            if(s.charAt(left) != s.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-    public static void main(String[] args){
-        String s = "abccba";
-        validpalindrom vp = new validpalindrom();
-        System.out.println(vp.ispalisndrome(s));
-    }
-}
+// public class validpalindrom{
+//     public boolean ispalisndrome(String s){
+//         int left = 0;
+//         int right = s.length() - 1;
+//         while(left < right){
+//             if(s.charAt(left) != s.charAt(right)){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+//     public static void main(String[] args){
+//         String s = "abccba";
+//         validpalindrom vp = new validpalindrom();
+//         System.out.println(vp.ispalisndrome(s));
+//     }
+// }
