14a0416f6b45357c98c870a349e9c913e23c409f
Shivam <mishrashivam20197@gmail.com>
2026-07-08 20:01:16 +0530

    update

--- Kadanes algorithm/buyorsell.java
+++ Kadanes algorithm/buyorsell.java
@@ -1,19 +1,18 @@
-public class buyorsell {
-    public int besttimetobuyorsell(int[] prices){
-        int minprice = prices[0];
-        int maxprofit = 0;
-        for(int i = 1; i<prices.length; i++){
-            minprice = Math.min(minprice, prices[i]);
-            int profit = prices[i] - minprice;
-            maxprofit = Math.max(profit, maxprofit);
-        }
-        return maxprofit;
-    }
+// public class buyorsell {
+//     public int besttimetobuyorsell(int[] prices){
+//         int minprice = prices[0];
+//         int maxprofit = 0;
+//         for(int i = 1; i<prices.length; i++){
+//             minprice = Math.min(minprice, prices[i]);
+//             int profit = prices[i] - minprice;
+//             maxprofit = Math.max(profit, maxprofit);
+//         }
+//         return maxprofit;
+//     }
 
-    public static void main(String[] args) {
-        buyorsell b = new buyorsell();
-        int[] prices = {7,1,5,3,6,4};
-        System.out.println(b.besttimetobuyorsell(prices));
-    }
-}
-made new changee
+//     public static void main(String[] args) {
+//         buyorsell b = new buyorsell();
+//         int[] prices = {7,1,5,3,6,4};
+//         System.out.println(b.besttimetobuyorsell(prices));
+//     }
+// }
