966a5c102fbba0610525ad606277cbdf60fb6ea0
Shivam <mishrashivam20197@gmail.com>
2026-07-15 20:33:44 +0530

    middle node

--- Linked list/middlenode.java
+++ Linked list/middlenode.java
@@ -1,36 +1,36 @@
-// class ListNode {
-//     int val;
-//     ListNode next;
+class ListNode {
+    int val;
+    ListNode next;
 
-//     ListNode(int x) {
-//         val = x;
-//         next = null;
-//     }
-// }
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
 
-// public class middlenode{
-//     public ListNode middleNode(ListNode head){
-//         if(head == null)return null;
-//         ListNode slow = head;
-//         ListNode fast = head;
+public class middlenode{
+    public ListNode middleNode(ListNode head){
+        if(head == null)return null;
+        ListNode slow = head;
+        ListNode fast = head;
 
-//         while(fast != null && fast.next != null){
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-//         return slow;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
 
-//     }
-//     public static void main(String[] args) {
+    }
+    public static void main(String[] args) {
 
-//         ListNode head = new ListNode(3);
-//         head.next = new ListNode(7);
-//         head.next.next = new ListNode(5);
-//         head.next.next.next = new ListNode(2);
-//         // head.next.next.next.next = new ListNode(1);
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(7);
+        head.next.next = new ListNode(5);
+        head.next.next.next = new ListNode(2);
+        // head.next.next.next.next = new ListNode(1);
 
-//         middlenode obj = new middlenode();
-//         ListNode middle = obj.middleNode(head);
-//         System.out.println("Middle node value: " + middle.val);
-//     }
-// }
+        middlenode obj = new middlenode();
+        ListNode middle = obj.middleNode(head);
+        System.out.println("Middle node value: " + middle.val);
+    }
+}
