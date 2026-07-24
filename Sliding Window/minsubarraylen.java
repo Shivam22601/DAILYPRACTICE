336c516f83d2b4fc2302b34510ca405596a4e357
Shivam <mishrashivam20197@gmail.com>
2026-07-24 23:07:20 +0530

    min subarray

--- Sliding Window/minsubarraylen.java
+++ Sliding Window/minsubarraylen.java
@@ -1,30 +1,30 @@
-// public class minsubarraylen{
-//     public int minsubarraylen(int target, int[] nums){
+public class minsubarraylen{
+    public int minsubarraylen(int target, int[] nums){
 
-//         int n = nums.length;
-//         int min_len = Integer.MAX_VALUE;
-//         int low = 0;
-//         int sum = 0;
-//         for(int high = 0 ; high <nums.length; high++){
-//             sum += nums[high];
-//             while(sum >= target){
-//                 min_len = Math.min(min_len, high - low + 1);
-//                 sum -= nums[low];
-//                 low++;
-//             }
-//         }
-//         if(min_len == Integer.MAX_VALUE){
-//             return 0;
-//         }else{
-//             return min_len;
-//         }
-//     }
-//     public static void main(String[] args) {
-//         minsubarraylen obj = new minsubarraylen();
-//         int target = 7;
-//         int[] nums = {2,3,1,2,4,3};
-//         System.out.println(obj.minsubarraylen(target, nums));        
-//     }
-// }
+        int n = nums.length;
+        int min_len = Integer.MAX_VALUE;
+        int low = 0;
+        int sum = 0;
+        for(int high = 0 ; high <nums.length; high++){
+            sum += nums[high];
+            while(sum >= target){
+                min_len = Math.min(min_len, high - low + 1);
+                sum -= nums[low];
+                low++;
+            }
+        }
+        if(min_len == Integer.MAX_VALUE){
+            return 0;
+        }else{
+            return min_len;
+        }
+    }
+    public static void main(String[] args) {
+        minsubarraylen obj = new minsubarraylen();
+        int target = 7;
+        int[] nums = {2,3,1,2,4,3};
+        System.out.println(obj.minsubarraylen(target, nums));        
+    }
+}
 
 
