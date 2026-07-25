8964af03ada97095160cd5ad6c3da9ecfb686173
Shivam <mishrashivam20197@gmail.com>
2026-07-25 19:43:25 +0530

    reverse string

--- Stack/reverseString.java
+++ Stack/reverseString.java
@@ -1,25 +1,25 @@
-// import java.util.Stack;
-// // import java.util.String;
-// public class reverseString {
-//     public static String reverse(String str){
-//         Stack<Character> stack = new Stack<>();
-//         for(int i = 0; i < str.length() ;i++){
-//             stack.push(str.charAt(i));
-//         }
-//         String reversed = "";
-//         while(!stack.isEmpty()){
-//             reversed += stack.pop();
-//         }
-//         return reversed;
-//     }
+import java.util.Stack;
+// import java.util.String;
+public class reverseString {
+    public static String reverse(String str){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < str.length() ;i++){
+            stack.push(str.charAt(i));
+        }
+        String reversed = "";
+        while(!stack.isEmpty()){
+            reversed += stack.pop();
+        }
+        return reversed;
+    }
 
-//     public static void main(String[] args) {
-//         Stack<Integer> stack = new Stack<>();
-//         String str = "Hello";
-//         String result = reverse(str);
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        String str = "Hello";
+        String result = reverse(str);
 
-//         System.out.println("Reversed string: " + result);
+        System.out.println("Reversed string: " + result);
 
         
-//     }
-// }
+    }
+}
