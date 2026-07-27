35a609c87ce37ed65b2b2743d935f80c87c78a41
Shivam <mishrashivam20197@gmail.com>
2026-07-27 20:30:42 +0530

    update

--- Binary search/searchcharlessthantarget.java
+++ Binary search/searchcharlessthantarget.java
@@ -1,22 +1,22 @@
-public class searchcharlessthantarget{
-     public char nextGreatestLetter(char[] letters, char target) {
-        int left = 0;
-        int right = letters.length - 1;
-        while(left <= right){
-            int mid = left + (right - left)/2;
-           if(letters[mid] <= target){
-                left = mid + 1;
-            }else{
-                right = mid - 1;
-            }
-        }
-        return letters[left % letters.length];
-     }
-     public static void main(String[] args){
-        char[] letters = {'c', 'f', 'j'};
-        char target = 'a';
-        searchcharlessthantarget obj = new searchcharlessthantarget();
-        char result = obj.nextGreatestLetter(letters, target);
-        System.out.println(result);
-     }
-}
+// public class searchcharlessthantarget{
+//      public char nextGreatestLetter(char[] letters, char target) {
+//         int left = 0;
+//         int right = letters.length - 1;
+//         while(left <= right){
+//             int mid = left + (right - left)/2;
+//            if(letters[mid] <= target){
+//                 left = mid + 1;
+//             }else{
+//                 right = mid - 1;
+//             }
+//         }
+//         return letters[left % letters.length];
+//      }
+//      public static void main(String[] args){
+//         char[] letters = {'c', 'f', 'j'};
+//         char target = 'a';
+//         searchcharlessthantarget obj = new searchcharlessthantarget();
+//         char result = obj.nextGreatestLetter(letters, target);
+//         System.out.println(result);
+//      }
+// }
