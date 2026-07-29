95b605ba3fca02d4b75c3521b0c29308be4843b6
Shivam <mishrashivam20197@gmail.com>
2026-07-29 20:41:26 +0530

    update

--- Hashmap/prefixsum.java
+++ Hashmap/prefixsum.java
@@ -1,25 +1,25 @@
 
-import java.util.HashMap;
-public class prefixsum{
-    public static int[] prefixsum(int arr[]){
-    HashMap<Integer, Integer> map = new HashMap<>();
-    int sum = 0 ;
-    for(int i = 0 ; i< arr.length ; i++){
-        sum += arr[i];
-        map.put(i, sum);
-    }
-    int prefix[] = new int[arr.length];
-    for(int i = 0 ; i < arr.length ; i++){
-        prefix[i] = map.get(i);
-    }
-    return prefix;
-}
+// import java.util.HashMap;
+// public class prefixsum{
+//     public static int[] prefixsum(int arr[]){
+//     HashMap<Integer, Integer> map = new HashMap<>();
+//     int sum = 0 ;
+//     for(int i = 0 ; i< arr.length ; i++){
+//         sum += arr[i];
+//         map.put(i, sum);
+//     }
+//     int prefix[] = new int[arr.length];
+//     for(int i = 0 ; i < arr.length ; i++){
+//         prefix[i] = map.get(i);
+//     }
+//     return prefix;
+// }
 
-public static void main(String[] args) {
-    int arr[] = {1, 2, 3, 4, 5};
-    int prefix[] = prefixsum(arr);
-    for(int i = 0 ; i < prefix.length ; i++){
-        System.out.print(prefix[i] + " ");
-    }
-}
-}
+// public static void main(String[] args) {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int prefix[] = prefixsum(arr);
+//     for(int i = 0 ; i < prefix.length ; i++){
+//         System.out.print(prefix[i] + " ");
+//     }
+// }
+// }
