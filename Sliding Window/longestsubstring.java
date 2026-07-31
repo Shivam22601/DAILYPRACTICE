4e032caa1feecf57a99e0cb2b953f81267d593ee
Shivam <mishrashivam20197@gmail.com>
2026-07-31 19:53:42 +0530

    longest substring

--- Sliding Window/longestsubstring.java
+++ Sliding Window/longestsubstring.java
@@ -1,27 +1,27 @@
-// import java.util.HashSet;
-// import java.util.Set;
-// public class longestsubstring{
-//      public int lengthOfLongestSubstring(String s) {
-//         int maxLen = 0;
-//         int left = 0;
+import java.util.HashSet;
+import java.util.Set;
+public class longestsubstring{
+     public int lengthOfLongestSubstring(String s) {
+        int maxLen = 0;
+        int left = 0;
         
-//         Set<Character> charSet = new HashSet<>();
+        Set<Character> charSet = new HashSet<>();
         
-//         for (int right = 0; right < s.length(); right++) {
-//             char currentChar = s.charAt(right);
-//             while (charSet.contains(currentChar)) {
-//                 charSet.remove(s.charAt(left));
-//                 left++;
-//             }
-//             charSet.add(currentChar);
-//             maxLen = Math.max(maxLen, right - left + 1);
-//         }
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+            while (charSet.contains(currentChar)) {
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+            charSet.add(currentChar);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
         
-//         return maxLen;
-//     }
-//     public static void main(String[] args) {
-//         longestsubstring solution = new longestsubstring();
-//         String s = "abcabcbb";
-//         System.out.println(solution.lengthOfLongestSubstring(s));
-//     }
-// }
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        longestsubstring solution = new longestsubstring();
+        String s = "abcabcbb";
+        System.out.println(solution.lengthOfLongestSubstring(s));
+    }
+}
