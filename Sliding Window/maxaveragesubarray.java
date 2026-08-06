7790d7e3bf0819c348a89b4270ebad8ccb8839ac
Shivam <mishrashivam20197@gmail.com>
2026-08-06 21:27:03 +0530

    update

--- Sliding Window/maxaveragesubarray.java
+++ Sliding Window/maxaveragesubarray.java
@@ -1,30 +1,30 @@
-public class maxaveragesubarray {
-    public double findMaxAverage(int[] nums, int k) {
-     int n = nums.length;
+// public class maxaveragesubarray {
+//     public double findMaxAverage(int[] nums, int k) {
+//      int n = nums.length;
 
-        if(k > n) return -1;
+//         if(k > n) return -1;
 
-        int sum = 0;
+//         int sum = 0;
 
-        // First window sum
-        for(int i = 0; i < k; i++){
-            sum += nums[i];
-        }
+//         // First window sum
+//         for(int i = 0; i < k; i++){
+//             sum += nums[i];
+//         }
 
-        int maxsum = sum;
+//         int maxsum = sum;
 
-        // Sliding window
-        for(int i = k; i < n; i++){
-            sum = sum - nums[i - k] + nums[i];
-            maxsum = Math.max(sum, maxsum);
-        }
+//         // Sliding window
+//         for(int i = k; i < n; i++){
+//             sum = sum - nums[i - k] + nums[i];
+//             maxsum = Math.max(sum, maxsum);
+//         }
 
-        return (double) maxsum / k;
-    }
-    public static void main(String[] args) {
-        maxaveragesubarray solution = new maxaveragesubarray();
-        int[] nums = {1, 12, -5, -6, 50, 3};
-        int k = 4;
-        System.out.println(solution.findMaxAverage(nums, k));
-    }
-}
+//         return (double) maxsum / k;
+//     }
+//     public static void main(String[] args) {
+//         maxaveragesubarray solution = new maxaveragesubarray();
+//         int[] nums = {1, 12, -5, -6, 50, 3};
+//         int k = 4;
+//         System.out.println(solution.findMaxAverage(nums, k));
+//     }
+// }
