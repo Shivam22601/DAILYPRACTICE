0f8f7b12e4d7248339e2cc3a6c53808d6984c908
Shivam <mishrashivam20197@gmail.com>
2026-08-10 20:35:24 +0530

    search insert

--- Binary search/searchinsert.java
+++ Binary search/searchinsert.java
@@ -1,25 +1,25 @@
-// public class searchinsert{
-//     public int find(int[] nums, int target){
-//         int left = 0;
-//         int right = nums.length - 1;
-//         while(left <= right){
-//             int mid = left + (right - left)/2;
-//             if(nums[mid] == nums[right]){
-//                 return mid;
-//             }
-//             else if(nums[mid] < target){
-//                 left = mid + 1;
-//             }
-//             else{
-//                 right = mid - 1;
-//             }
-//         }
-//         return left;
-//     }
-//     public static void main(String[] args){
-//         int[] nums = {1,3,4,5};
-//         int target = 2;
-//         searchinsert s = new searchinsert();
-//         System.out.println(s.find(nums, target));
-//     }
-// }
+public class searchinsert{
+    public int find(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] == nums[right]){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+    public static void main(String[] args){
+        int[] nums = {1,3,4,5};
+        int target = 2;
+        searchinsert s = new searchinsert();
+        System.out.println(s.find(nums, target));
+    }
+}
