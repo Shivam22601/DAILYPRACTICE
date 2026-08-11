0cbcc82b562fcf545f159de651d908b737102055
Shivam <mishrashivam20197@gmail.com>
2026-08-11 20:46:50 +0530

    update

--- Hashmap/validparanthesis.java
+++ Hashmap/validparanthesis.java
@@ -1,27 +1,27 @@
-import java.util.Stack;
-public class validparanthesis{
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
-    }
-    public static void main(String[] args) {
-        validparanthesis vp = new validparanthesis();
-        String s = "({[]})";
-        System.out.println(vp.isValid(s)); // Output: true
-    }
-}
+// import java.util.Stack;
+// public class validparanthesis{
+//     public boolean isValid(String s) {
+//         Stack<Character> stack = new Stack<>();
+//         for (char c : s.toCharArray()) {
+//             if (c == '(' || c == '{' || c == '[') {
+//                 stack.push(c);
+//             } else {
+//                 if (stack.isEmpty()) {
+//                     return false;
+//                 }
+//                 char top = stack.pop();
+//                 if ((c == ')' && top != '(') ||
+//                     (c == '}' && top != '{') ||
+//                     (c == ']' && top != '[')) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+//     public static void main(String[] args) {
+//         validparanthesis vp = new validparanthesis();
+//         String s = "({[]})";
+//         System.out.println(vp.isValid(s)); // Output: true
+//     }
+// }
