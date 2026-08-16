462fd9f5f801018c64d1ae8f7a4623d7e513a11b
Shivam <mishrashivam20197@gmail.com>
2026-08-16 20:58:02 +0530

    two sum

--- Two Pointers/twosum.java
+++ Two Pointers/twosum.java
@@ -1,28 +1,28 @@
-// import java.util.Arrays;
-// public class twosum{
-//     public int[] twosum(int[] nums, int target){
-//         int left = 0;
-//         int right = nums.length - 1;
-//         while(left < right){
-//             int sum = nums[left] + nums[right];
-//             if(sum == target){
-//                 return new int[]{nums[left], nums[right]};
-//             }
-//             else if(nums[left] + nums[right] < target){
-//                 left++;
-//             }
-//             else{
-//                 right--;
-//             }
-//         }
-//         int[] notFound = {-1, -1};
-//         return notFound;
-//     }
+import java.util.Arrays;
+public class twosum{
+    public int[] twosum(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            if(sum == target){
+                return new int[]{nums[left], nums[right]};
+            }
+            else if(nums[left] + nums[right] < target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        int[] notFound = {-1, -1};
+        return notFound;
+    }
     
-//     public static void main(String[] args){
-//         int[] nums = new int[]{2,3,7,11};
-//         int target = 9;
-//         twosum solution = new twosum();
-//         System.out.println(Arrays.toString(solution.twosum(nums, target)));
-//     }
-// }
+    public static void main(String[] args){
+        int[] nums = new int[]{2,3,7,11};
+        int target = 9;
+        twosum solution = new twosum();
+        System.out.println(Arrays.toString(solution.twosum(nums, target)));
+    }
+}
