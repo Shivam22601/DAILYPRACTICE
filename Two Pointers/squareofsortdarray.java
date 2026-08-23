1bd02c3a5e5389d2970e96810aab7338ee3788b9
Shivam <mishrashivam20197@gmail.com>
2026-08-23 20:50:37 +0530

    square duplicate

--- Two Pointers/squareofsortdarray.java
+++ Two Pointers/squareofsortdarray.java
@@ -1,29 +1,29 @@
-// public class squareofsortdarray {
-//     public static void main(String[] args) {
-//         int[] arr = {-4, -1, 0, 3, 10};
-//         int[] result = sortedSquares(arr);
-//         for (int num : result) {
-//             System.out.print(num + " ");
-//         }
-//     }
+public class squareofsortdarray {
+    public static void main(String[] args) {
+        int[] arr = {-4, -1, 0, 3, 10};
+        int[] result = sortedSquares(arr);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
 
-//     public static int[] sortedSquares(int[] nums) {
-//         int n = nums.length;
-//         int[] result = new int[n];
-//         int left = 0;
-//         int right = n - 1;
-//         int index = n - 1;
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
 
-//         while (left <= right) {
-//             if (Math.abs(nums[left]) > Math.abs(nums[right])) {
-//                 result[index--] = nums[left] * nums[left];
-//                 left++;
-//             } else {
-//                 result[index--] = nums[right] * nums[right];
-//                 right--;
-//             }
-//         }
+        while (left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[index--] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[index--] = nums[right] * nums[right];
+                right--;
+            }
+        }
 
-//         return result;
-//     }
-// }
+        return result;
+    }
+}
