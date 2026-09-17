c8f1f3f2456c8f5219f82144b7cae37c2ef2207f
Shivam <mishrashivam20197@gmail.com>
2026-09-17 21:00:23 +0530

    delete node

--- Linked list/deletenode.java
+++ Linked list/deletenode.java
@@ -1,22 +1,22 @@
-// class ListNode{
-//     int val;
-//     ListNode next;
-//     ListNode(int x){
-//         val = x;
-//         next = null;
-//     }
-// }
-// public class deletenode{
-//     public void deleteNode(ListNode node) {
-//         node.val = node.next.val;
-//         node.next = node.next.next;
-//     }
-//     public static void main(String[] args){
-//         ListNode head = new ListNode(4);
-//         head.next = new ListNode(5);
-//         head.next.next = new ListNode(1);
-//         head.next.next.next = new ListNode(9);
-//         deletenode obj = new deletenode();
-//         obj.deleteNode(head.next);
-//     }
-// }
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+        next = null;
+    }
+}
+public class deletenode{
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+    public static void main(String[] args){
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+        deletenode obj = new deletenode();
+        obj.deleteNode(head.next);
+    }
+}
